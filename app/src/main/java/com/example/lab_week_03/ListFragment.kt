1939291d3fd9863282c569c3f6c0b4a96596223c
Willbert Budi Lian <willbert.budi@student.umn.ac.id>
2025-09-12 15:20:37 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -2,12 +2,11 @@ package com.example.lab_week_03
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,56 +15,56 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class ListFragment : Fragment(), View.OnClickListener {
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var coffeeListener: CoffeeListener // Make sure CoffeeListener interface is defined
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        Log.d(TAG, "onAttach")
+        if (context is CoffeeListener) {
+            coffeeListener = context
+        } else {
+            throw RuntimeException("$context must implement CoffeeListener")
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG,"onCreate")
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.d(TAG,"onCreateView")
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        // Make sure you have a layout file, e.g., res/layout/fragment_list.xml
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated")
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+        coffeeList.forEach {
+            it.setOnClickListener(this)
+        }
     }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
+
+    override fun onClick(v: View?) {
+        v?.let { coffee ->
+            coffeeListener.onSelected(coffee.id)
+        }
     }
 
     companion object {
@@ -75,17 +74,16 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment ListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            ListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-        private const val TAG = "MainFragment"
     }
 }
